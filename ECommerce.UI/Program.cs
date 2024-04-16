4f527b3b349d2718caacb38525c7b76ab6730f37
halil <halilomersoysal@gmail.com>
2024-04-16 12:34:13 +0300

    Entties were created, database connection was made and migration was started.

--- ECommerce.UI/Program.cs
+++ ECommerce.UI/Program.cs
@@ -1,7 +1,12 @@
+using ECommerce.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
 
 var app = builder.Build();
 
